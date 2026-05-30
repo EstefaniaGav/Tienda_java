8d851b9debee5de701bff18f3826399e0a4bbe63
Sebastian Muñoz <sebastianmunozz93@gmail.com>
2026-05-30 11:05:41 -0500

    Se realiza entrega de la entidad rol y la dependencia Lombok

--- src/main/java/entities/Rol.java
+++ src/main/java/entities/Rol.java
@@ -1,5 +1,18 @@
 package entities;
 
-public class Rol {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer IdRol;
+    private String nombre;
 }
