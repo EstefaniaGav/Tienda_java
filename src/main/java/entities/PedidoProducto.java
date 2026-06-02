99d99d26359623ff595ee18f5318677cf75bd7e4
Sebastian M <sebastianmunozz93@gmail.com>
2026-06-01 21:12:59 -0500

    Se completan las entidades Pedido y PedidoProducto

--- src/main/java/entities/PedidoProducto.java
+++ src/main/java/entities/PedidoProducto.java
@@ -1,5 +1,22 @@
 package entities;
 
-public class PedidoProducto {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "pedidoproductos")
+public class PedidoProducto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer IdPedidoProducto;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "FK_IdPedido", referencedColumnName = "IdPedido", nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "FK_IdProducto", referencedColumnName = "IdProducto", nullable = false)
+    private Producto producto;
+    
+    @Column(nullable = false)
+    private Integer cantidadCompra;
 }
